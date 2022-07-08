5b2ad611743764041289f4adaab0ad1936866c4c
iotpainter <809953315@qq.com>
2022-07-08 19:35:37 +0800

    add iconfont and router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Element_Plus from 'element-plus' //引入elment-UI
+import 'element-plus/dist/index.css'
+import './assets/iconfont/iconfont' //引入iconfont字体图标库
+import './assets/iconfont/iconfont.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Element_Plus)
+app.mount('#app')
